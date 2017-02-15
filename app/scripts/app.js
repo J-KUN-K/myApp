280984f3bcc02f7391eccd36de30e0f72b25c208
tigerJK <mentor5520@naver.com>
2017-02-15 17:55:39 +0900

    about 페이지 추가 - app 에 라우트 의존성 추가 -index 라우팅 정보 표시

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,22 @@
  * Main module of the application.
  */
 angular
-  .module('sampleAppApp', []);
+  .module('sampleAppApp', ['ngRoute']).
+  config(function ($routeProvider) {
+  	$routeProvider
+  	.when('/main', {templateUrl : '/views/main.html', controller : 'mainCtrl' })
+  	.when('/about', {templateUrl : '/views/about.html', controller : 'aboutCtrl'})
+  	.otherwise({redirectTo: '/main'});
+  })
+  .controller('mainCtrl', function ($scope, $route) {
+  	$scope.route = $route;
+  	$scope.routes = $route.routes;
+  	$scope.$on("$routeChangeSuccess", function (e, cRoute, pRoute) {
+  		console.log("현재 라우팅 정보 : ", cRoute.loadedTemplateUrl);
+  		if(pRoute) console.log("이전 라우트 정보 : ", pRoute.loadedTemplateUrl);
+  	});
+  	/*
+  	$scope.reload = function () {
+  		$route.reload();
+  	};*/
+  });
